a8e323cc48cfaa6c5fae3edeeff53d92dd788a30
kkznch <kkznch710md@gmail.com>
2023-07-27 22:25:58 +0900

    Adding a Router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// eslint-disable-next-line import/namespace
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from '@/App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
