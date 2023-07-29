3fe40b219aa37c0578393b36bab7f26ed3eb949b
kkznch <kkznch710md@gmail.com>
2023-07-29 18:02:03 +0900

    Handling Not Found Errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 // eslint-disable-next-line import/namespace
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import ErrorPage from './error-page';
 import Root from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
