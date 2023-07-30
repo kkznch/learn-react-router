c0ef15cf3856d835253efbb7e12eac111b5c508b
kkznch <kkznch710md@gmail.com>
2023-07-30 16:08:34 +0900

    Creating Contacts

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
 import Contact from './routes/contact';
-import Root, { loader as rootLoader } from './routes/root';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 
 const router = createBrowserRouter([
   {
@@ -12,6 +12,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: 'contacts/:contactId',
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,6 +1,11 @@
 // eslint-disable-next-line import/namespace
-import { Link, Outlet, useLoaderData } from 'react-router-dom';
-import { getContacts } from '../contacts';
+import { Link, Outlet, useLoaderData, Form } from 'react-router-dom';
+import { getContacts, createContact } from '../contacts';
+
+export const action = async () => {
+  const contact = await createContact();
+  return { contact };
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
@@ -25,9 +30,9 @@ const Root = () => {
             <div id="search-spinner" aria-hidden hidden={true} />
             <div className="sr-only" aria-live="polite"></div>
           </form>
-          <form method="post">
+          <Form method="post">
             <button type="submit">New</button>
-          </form>
+          </Form>
         </div>
         <nav>
           {contacts.length ? (
