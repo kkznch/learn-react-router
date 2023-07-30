256598a8c1d69d4c63a0019020c58fe5cc8522f4
kkznch <kkznch710md@gmail.com>
2023-07-30 13:42:28 +0900

    The Contact Route UI

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import { createRoot } from 'react-dom/client';
 // eslint-disable-next-line import/namespace
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ErrorPage from './error-page';
+import Contact from './routes/contact';
 import Root from './routes/root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
     errorElement: <ErrorPage />,
   },
 ]);
